bae89da642dc12050dca0fa38dd63fcda37200ee
Bertrand Fan <bertrand@fan.net>
2013-09-13 09:13:39 -0700

    Never end a sentence with "a", variable length couplets

--- app.js
+++ app.js
@@ -10,31 +10,41 @@ var secrets = require(__dirname + '/secrets.json');
 
 m.seed(s, function () {
 
-	var generateLine = function() {
+	var generateLine = function(maxLength) {
 
 		var line = '';
 
+		var picked_words = [];
 		var words = m.fill(m.pick());
+
 		for (var i = 0; i < words.length; i++) {
 
-			var sentence = line + (line === "" ? "" : " ") + words[i];
-			if (sentence.length > 68) {
+			picked_words.push(words[i]);
+
+			if (picked_words.join(" ").length > maxLength) {
+				picked_words.pop();
 				break;
-			} else {
-				line = sentence;
 			}
 
 		}
+
+		if (picked_words[picked_words.length - 1] === "a") {
+			picked_words.pop();
+		}
+
+		line = picked_words.join(" ");
 
 		return line;
 
 	}
 
-	var tweet = generateLine() + ' / ' + generateLine();
+	var maxLength1 = Math.floor(Math.random()*(98-38+1)+38);
+	var maxLength2 = 140 - 3 - maxLength1;
+
+	var tweet = generateLine(maxLength1) + ' / ' + generateLine(maxLength2);
 
 	var twitter = require('twitter');
 	var twit = new twitter(secrets);
-
 
 	console.log('Tweeting "' + tweet + '"');
 
